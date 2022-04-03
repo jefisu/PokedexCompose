f020b6a9805f07eed00266a72cf4d545f56c462d
Jeferson Coutinho <jefissuu@gmail.com>
2022-04-03 12:21:03 -0300

    Add image that is displayed if the favorites list is empty

--- app/src/main/java/com/jefisu/pokedexcompose/feature_pokedex/presentation/favorites/FavoritesListScreen.kt
+++ app/src/main/java/com/jefisu/pokedexcompose/feature_pokedex/presentation/favorites/FavoritesListScreen.kt
@@ -1,22 +1,22 @@
 package com.jefisu.pokedexcompose.feature_pokedex.presentation.favorites
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jefisu.pokedexcompose.R
 import com.jefisu.pokedexcompose.core.components.PokemonItem
 import com.jefisu.pokedexcompose.core.components.TopBar
+import com.jefisu.pokedexcompose.core.presentation.StandardLoadingErrorScreen
 import com.jefisu.pokedexcompose.core.util.Screen
-import com.jefisu.pokedexcompose.ui.theme.spacing
 
 @ExperimentalMaterialApi
 @Composable
@@ -30,29 +30,21 @@ fun FavoritesListScreen(
             title = stringResource(R.string.favorites),
             onClickIcon1 = navController::navigateUp,
         )
-        LazyColumn {
-            items(
-                items = state.pokemonsFav
-            ) { pokemon ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(state.pokemonsFav) { pokemon ->
                 PokemonItem(
                     pokemonInfo = pokemon,
                     onClick = {
                         navController.navigate(Screen.Detail.navArgs(pokemon.name))
                     }
                 )
-                Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
             }
         }
     }
-    if (state.pokemonsFav.isEmpty()) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = stringResource(R.string.there_are_no_saved_items),
-                style = MaterialTheme.typography.h5
-            )
-        }
-    }
+    StandardLoadingErrorScreen(
+        hasError = state.pokemonsFav.isEmpty(),
+        errorMessage = stringResource(R.string.there_are_no_saved_items)
+    )
 }
